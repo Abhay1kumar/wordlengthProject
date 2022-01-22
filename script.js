6bd7404e3b820b3dba8d933b6f01f4d6bf19f9dc
Abhay1kumar <kumarabhay401@gmail.com>
2022-01-23 00:18:12 +0530

    myguessgame

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let button = document.getElementById('btn');
+let x = Math.floor(Math.random() *10+1);
+console.log(x);
+let button=document.getElementById('btn');
 button.addEventListener('click',function(){
-    let CountryName= document.getElementById("txt").value;
-    
-    console.log(CountryName);
-    let creatNewOption=document.createElement("option");
-    creatNewOption.text=CountryName;
-    let select=document.getElementById("select");
-    select.appendChild(creatNewOption);
-
-});
+    let name = document.getElementById('name').value;
+    let y=document.getElementById('number').value;
+    console.log(y);
+    if(x==y){
+        alert('Congratulation👌😀 you guessed right number ' + name);
+    }else{
+        alert('😭Try again. You guessed worng Mr.' + name);
+    }
+})
